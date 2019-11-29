402909367074a1366cdf329b23461c12590a479f
Henry Addison <henryaddison@users.noreply.github.com>
2019-11-29 15:43:21 +0000

    add the dataset using the lib

--- app.rb
+++ app.rb
@@ -3,9 +3,34 @@ require 'sinatra/json'
 require 'sinatra/reloader' if development?
 
 require 'openactive'
+require 'openactive/dataset'
 
 get '/' do
-  "Hello World! #{request.base_url}/feed/session-series for a feed"
+  feed_types = [
+    OpenActive::Dataset::FeedType::SESSION_SERIES,
+  ]
+
+  settings = OpenActive::Dataset::Settings.new(
+    open_data_feed_base_url: request.base_url + "/feed/",
+    dataset_site_url: request.base_url,
+    dataset_discussion_url: "https://github.com/simpleweb/sw-oa-ruby-test-site",
+    dataset_documentation_url: "https://developer.openactive.io/",
+    dataset_languages: ["en-GB"],
+    organisation_name: "Simpleweb",
+    organisation_url: "https://simpleweb.co.uk/",
+    organisation_legal_entity: "Simpleweb Ltd",
+    organisation_plain_text_description: "Simpleweb is a purpose driven software company that specialises in new "\
+                                         "technologies, product development, and human interaction.",
+    organisation_logo_url: "https://simpleweb.co.uk/wp-content/uploads/2015/07/facebook-default.png",
+    organisation_email: "spam@simpleweb.co.uk",
+    background_image_url: "https://simpleweb.co.uk/wp-content/uploads/2017/06/IMG_8994-500x500-c-default.jpg",
+    date_first_published: "2019-11-05", # remember, remember the fifth of November...
+    data_feed_types: feed_types,
+  )
+
+  renderer = OpenActive::Dataset::TemplateRenderer.new(settings)
+
+  renderer.render
 end
 
 disable :strict_paths
